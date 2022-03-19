d9eae8f709e40003efe1b7dc0c66c176268e2da9
CoderPopCat <rguy6085@gmail.com>
2022-03-19 15:08:12 +0400

    clear up

--- assets/js/script.js
+++ assets/js/script.js
@@ -61,14 +61,6 @@ projects.forEach((project) => {
   		</div>`
 })
 
-// Nav
-
-const links = document.querySelectorAll(".nav-link");
-Array.from(links).forEach(link => {
-	if (link.getAttribute('data-scroll')) {
-		link.onclick = () => window.scrollTo({ top: document.querySelector(link.getAttribute("data-scroll")).offsetTop, behavior: 'smooth' })
-	}
-})
 
 // Blob
 const renderer = new THREE.WebGLRenderer({ canvas: document.getElementById('canvas'), antialias: true });
@@ -98,3 +90,151 @@ function animate() {
 	requestAnimationFrame(animate);
 }
 requestAnimationFrame(animate);
+
+// Nav
+
+const links = document.querySelectorAll(".nav-link");
+Array.from(links).forEach(link => {
+	if (link.getAttribute('data-scroll')) {
+		link.onclick = () => window.scrollTo({ top: document.querySelector(link.getAttribute("data-scroll")).offsetTop, behavior: 'smooth' })
+	}
+})
+window.addEventListener('resize', add)
+function add() {
+	if (window.innerWidth < 860) {
+		document.body.classList.add('mobile')
+	} else {
+		document.body.classList.remove('mobile')
+	}
+}
+window.onload = add;
+let hamburger = document.querySelector('.hamburger')
+let mobileNav = document.querySelector('.nav-list')
+let bars = document.querySelectorAll('.hamburger span')
+let isActive = false
+hamburger.addEventListener('click', function () {
+	mobileNav.classList.toggle('open')
+	if (!isActive) {
+		bars[0].style.transform = 'rotate(45deg)'
+		bars[1].style.opacity = '0'
+		bars[2].style.transform = 'rotate(-45deg)'
+		isActive = true
+	} else {
+		bars[0].style.transform = 'rotate(0deg)'
+		bars[1].style.opacity = '1'
+		bars[2].style.transform = 'rotate(0deg)'
+		isActive = false
+	}
+})
+
+// Particles
+particlesJS("bg", {
+	"particles": {
+		"number": {
+			"value": 134,
+			"density": {
+				"enable": true,
+				"value_area": 800
+			}
+		},
+		"color": {
+			"value": "#ffffff"
+		},
+		"shape": {
+			"type": "circle",
+			"stroke": {
+				"width": 0,
+				"color": "#000000"
+			},
+			"polygon": {
+				"nb_sides": 5
+			},
+			"image": {
+				"src": "img/github.svg",
+				"width": 100,
+				"height": 100
+			}
+		},
+		"opacity": {
+			"value": 0.5,
+			"random": false,
+			"anim": {
+				"enable": false,
+				"speed": 1,
+				"opacity_min": 0.1,
+				"sync": false
+			}
+		},
+		"size": {
+			"value": 3,
+			"random": true,
+			"anim": {
+				"enable": false,
+				"speed": 40,
+				"size_min": 0.1,
+				"sync": false
+			}
+		},
+		"line_linked": {
+			"enable": true,
+			"distance": 150,
+			"color": "#ffffff",
+			"opacity": 0.4,
+			"width": 1
+		},
+		"move": {
+			"enable": true,
+			"speed": 6,
+			"direction": "none",
+			"random": false,
+			"straight": false,
+			"out_mode": "out",
+			"bounce": false,
+			"attract": {
+				"enable": false,
+				"rotateX": 600,
+				"rotateY": 1200
+			}
+		}
+	},
+	"interactivity": {
+		"detect_on": "canvas",
+		"events": {
+			"onhover": {
+				"enable": true,
+				"mode": "repulse"
+			},
+			"onclick": {
+				"enable": true,
+				"mode": "push"
+			},
+			"resize": true
+		},
+		"modes": {
+			"grab": {
+				"distance": 400,
+				"line_linked": {
+					"opacity": 1
+				}
+			},
+			"bubble": {
+				"distance": 400,
+				"size": 40,
+				"duration": 2,
+				"opacity": 8,
+				"speed": 3
+			},
+			"repulse": {
+				"distance": 200,
+				"duration": 0.4
+			},
+			"push": {
+				"particles_nb": 4
+			},
+			"remove": {
+				"particles_nb": 2
+			}
+		}
+	},
+	"retina_detect": true
+})
